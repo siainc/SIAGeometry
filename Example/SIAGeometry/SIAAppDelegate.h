e42b8f2067d2fcc46a9d2fa744a0d7aaab4eb5da
KUROSAKI Ryota <kurosaki@si-agency.co.jp>
2014-10-06 17:10:01 +0900

    Initial commit

---
+++ Example/SIAGeometry/SIAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SIAAppDelegate.h
+//  SIAGeometry
+//
+//  Created by CocoaPods on 10/06/2014.
+//  Copyright (c) 2014 KUROSAKI Ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/SIAGeometry/SIAViewController.h
@@ -0,0 +1,13 @@
+//
+//  SIAViewController.h
+//  SIAGeometry
+//
+//  Created by KUROSAKI Ryota on 10/06/2014.
+//  Copyright (c) 2014 KUROSAKI Ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIAViewController : UIViewController
+
+@end
